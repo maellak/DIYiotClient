f3e4f1b989b8d7a6bba8a03e1f8202354462031a
devopen <opendevkar@gmail.com>
2014-10-23 17:33:04 +0300

    first version of swagger-php annotations

--- api/get/GetLs.php
+++ api/get/GetLs.php
@@ -1,7 +1,52 @@
 <?php
 
 header("Content-Type: text/html; charset=utf-8");
+/**
+*
+* @SWG\Resource(
+*   apiVersion="0.1",
+*   swaggerVersion="2.0",
+*   basePath="http://localhost:8888/api",
+*   resourcePath="/ls",
+*   description="Εκτελεση εντολής ls -la στον client",
+*   produces="['application/json']"
+* )
+*/
+/**
+ * @SWG\Api(
+ *   path="/ls",
+ *   @SWG\Operation(
+ *     method="GET",
+ *     summary="Αναζήτηση σε κατάλογο συστήματος",
+ *     notes="Επιστρέφει τον κατάλογο συστήματος",
+ *     type="pathModel",
+ *     nickname="find_catalog",
+ *     @SWG\Parameter(
+ *       name="path",
+ *       description="Πληκτρολογηστε το system path",
+ *       required=false,
+ *       type="text",
+ *       paramType="query"
+ *     ),
+ *     @SWG\ResponseMessage(code=200, message="Επιτυχία", responseModel="Success"),
+ *     @SWG\ResponseMessage(code=500, message="Αποτυχία", responseModel="Failure")
+ *   )
+ * )
+ *
+     */ 
 
+ /**
+ *
+ * @SWG\Model(
+ *              id="pathModel",
+ *              required="path",
+ *                  @SWG\Property(name="path",type="string",description="Το path")
+ * )
+ */
+
+/**
+ * 
+ */ 
 function GetLs($path) {
    
     global $app;
