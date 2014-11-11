ac917607e89dd618b5f8264404aece9671762e88
root apostolos <info.interpedia@gmail.com>
2014-11-11 13:07:26 -0500

    insert install

---
+++ api/server/system/includes.php
@@ -0,0 +1,14 @@
+<?php
+
+//system
+require_once('system/config.php');
+
+//exceptions
+require_once('exceptions/ExceptionCodes.php');
+require_once('exceptions/ExceptionMessages.php');
+require_once('exceptions/ExceptionManager.php');
+
+//api
+require_once('../api/get/GetLs.php');
+
+?>
